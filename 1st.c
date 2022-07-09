d12ac1ea1437d6231e289d129cd6aa19011ec9c9
Akshat <80572104+akshat-rana740@users.noreply.github.com>
2022-07-09 20:01:54 +0530

    1st.c
    This file is just for getting some basic functions in brain

---
+++ 1st.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//a program to input element in array
+int main(){
+  int arr[];
+  printf("Enter 10 numbers");
+  for(int i =1;i<=9;i++){
+    scanf("%d",arr[i]);
+  }
+  for(int i =1;i<=9;i++){
+    printf("The array elements are:%d",arr[i]);
+  }
+  }
